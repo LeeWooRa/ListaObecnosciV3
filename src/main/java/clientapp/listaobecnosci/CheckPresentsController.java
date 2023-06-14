5587ee0ecb9684895d95cc3c6bc342da14378264
LeeWooRa <weronika.pruska.wp@gmail.com>
2023-06-15 00:10:21 +0200

    check

--- src/main/java/clientapp/listaobecnosci/CheckPresentsController.java
+++ src/main/java/clientapp/listaobecnosci/CheckPresentsController.java
@@ -1,8 +1,6 @@
 package clientapp.listaobecnosci;
 
-import clientapp.listaobecnosci.Shared.Entities.Period;
-import clientapp.listaobecnosci.Shared.Entities.StudentGroup;
-import clientapp.listaobecnosci.Shared.Entities.Subject;
+import clientapp.listaobecnosci.Shared.Entities.*;
 import clientapp.listaobecnosci.Shared.Helpers.DataHandler.DataHandler;
 import clientapp.listaobecnosci.Shared.Helpers.JsonConverter;
 import clientapp.listaobecnosci.Shared.Helpers.ResponseHandler.ResponseHandler;
@@ -12,6 +10,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -21,6 +20,7 @@ import javafx.scene.control.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class CheckPresentsController implements Initializable {
@@ -32,10 +32,13 @@ public class CheckPresentsController implements Initializable {
     ChoiceBox<String> period;
     @FXML
     ListView<SplitMenuButton> list;
+    @FXML
+    Label resultMsg;
     private Parent root;
     private ArrayList<Period> periodList;
     private ArrayList<StudentGroup> groupList;
     private ArrayList<Subject> subjectList;
+    private ArrayList<PresenceVm> presenceList = new ArrayList<PresenceVm>();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         DataHandler<ArrayList<StudentGroup>> dh = new DataHandler<ArrayList<StudentGroup>>("GetStudentGroupList", null);
@@ -131,24 +134,77 @@ public class CheckPresentsController implements Initializable {
                 break;
             }
         }
-        DataHandler<Integer> dh = new DataHandler<Integer>("getPresenceList", periodId);
+        DataHandler<Integer> dh = new DataHandler<Integer>("GetPresenceList", periodId);
         String json = JsonConverter.convertClassToJson(dh);
         String respond = Utils.connectToServer(json);
         TypeReference<ResponseHandler<ArrayList<PresenceVm>>> typeReference = new TypeReference<ResponseHandler<ArrayList<PresenceVm>>>() {};
         ResponseHandler<ArrayList<PresenceVm>> dataHandler = JsonConverter.convertJsonToClass(respond, typeReference);
         if (dataHandler.isSuccess()) {
-            MenuItem choice1 = new MenuItem("Obecny");
-            MenuItem choice2 = new MenuItem("Nieobecny");
-            MenuItem choice3 = new MenuItem("Usprawiedliwiony");
-            MenuItem choice4 = new MenuItem("Spóźniony");
             ObservableList<SplitMenuButton> listElements = FXCollections.observableArrayList();
             for (PresenceVm pres : dataHandler.getData()) {
                 SplitMenuButton splitMenuButton = new SplitMenuButton();
+
+                MenuItem choice1 = new MenuItem("Obecny");
+                choice1.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        addToUpdateList(e, pres,splitMenuButton);
+                    }
+                });
+                MenuItem choice2 = new MenuItem("Nieobecny");
+                choice2.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        addToUpdateList(e, pres,splitMenuButton);
+                    }
+                });
+                MenuItem choice3 = new MenuItem("Usprawiedliwiony");
+                choice3.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        addToUpdateList(e, pres,splitMenuButton);
+                    }
+                });
+                MenuItem choice4 = new MenuItem("Spóźniony");
+                choice4.setOnAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent e) {
+                        addToUpdateList(e, pres,splitMenuButton);
+                    }
+                });
+
                 splitMenuButton.setText(pres.getStudentIndex()+" "+pres.getFirstName()+" "+pres.getLastName());
-                splitMenuButton.getItems().addAll(choice1, choice2, choice3);
+                splitMenuButton.getItems().addAll(choice1, choice2, choice3, choice4);
                 listElements.add(splitMenuButton);
             }
             list.setItems(listElements);
+        }
+    }
+
+    private void addToUpdateList(ActionEvent e, PresenceVm presence, SplitMenuButton smb){
+        MenuItem item = (MenuItem) e.getSource();
+        presence.setStatus(item.getText());
+        smb.setText(presence.getStudentIndex()+" "+presence.getFirstName()+" "+presence.getLastName()+" - "+presence.getStatus());
+        Boolean isDuplicated = false;
+        for (PresenceVm pres : presenceList){
+            if (pres.getLastName().equals(presence.getLastName()) && pres.getFirstName().equals(presence.getFirstName()) && pres.getStudentIndex().equals(presence.getStudentIndex())){
+                isDuplicated = true;
+                presence.setStatus(item.getText());
+            }
+        }
+
+        if(!isDuplicated){
+            presenceList.add(presence);
+        }
+    }
+
+    @FXML
+    protected void savePresense() throws Exception {
+        DataHandler<ArrayList<PresenceVm>> dh = new DataHandler<ArrayList<PresenceVm>>("UpdatePresence", presenceList);
+        String json = JsonConverter.convertClassToJson(dh);
+        String respond = Utils.connectToServer(json);
+        TypeReference<ResponseHandler<Boolean>> typeReference = new TypeReference<ResponseHandler<Boolean>>() {};
+        ResponseHandler<Boolean> dataHandler = JsonConverter.convertJsonToClass(respond, typeReference);
+        if (dataHandler.isSuccess()) {
+            resultMsg.setText("Zaktualizowano obecność");
+        } else {
+            resultMsg.setText("Coś poszło nie tak");
         }
     }
 }
